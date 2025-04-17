1c8f7dd99d820a398e9eedfbc06b5a02a9b71967
qqjeffrey <jeffrey890705@gmail.com>
2025-04-17 19:34:40 +0800

    test: test for get_random

--- app/service/quote_service.py
+++ app/service/quote_service.py
@@ -7,10 +7,13 @@ class QuoteService:
         self.repo = repo
 
     def add_quote(self, quote: Quote) -> Quote:
+        raise NotImplementedError()
         return self.repo.add(quote)
 
     def get_by_author(self, author: str) -> List[Quote]:
+        raise NotImplementedError()
         return self.repo.get_by_author(author)
 
     def get_random(self) -> Quote:
+        raise NotImplementedError()
         return self.repo.get_random()
